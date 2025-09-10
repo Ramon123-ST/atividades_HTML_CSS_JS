f00426f6d7f0e7bed2883e6779c3acb2ce8a68dc
anderson123-st <christianramonquadros@gmail.com>
2025-09-09 22:54:07 -0300

    Corrigir link da Atividade 8 para GitHub Pages

---
+++ lista_atividade_01/js/teste_js.js
@@ -0,0 +1,20 @@
+function exibe_bugatti(){
+    document.getElementById("bugatti").style.visibility="visible";
+    document.getElementById("ferrari").style.visibility="hidden";
+    document.getElementById("carro").style.visibility="hidden";
+}
+function exibe_ferrari(){
+    document.getElementById("bugatti").style.visibility="hidden";
+    document.getElementById("ferrari").style.visibility="visible";
+    document.getElementById("carro").style.visibility="hidden";
+}
+function exibe_carro(){
+    document.getElementById("bugatti").style.visibility="hidden";
+    document.getElementById("ferrari").style.visibility="hidden";
+    document.getElementById("carro").style.visibility="visible";
+}
+function some_tudo(){
+    document.getElementById("bugatti").style.visibility="hidden";
+    document.getElementById("ferrari").style.visibility="hidden";
+    document.getElementById("carro").style.visibility="hidden";
+}
